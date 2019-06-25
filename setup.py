f2dc0e5b5c15ad5f6845cd8c774acfcf437a352b
Jeffrey Cameron <jeffreycameron@gmail.com>
2019-06-25 09:16:57 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An examination of the changing population of Pictou County through publicly available data sets',
+    author='Jeffrey Cameron',
+    license='MIT',
+)
